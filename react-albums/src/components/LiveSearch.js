2e9d85db75957193454c39dce8412e6567a0ae96
elyeel <elyeel@yahoo.com>
2019-09-05 21:04:57 -0700

    live search solution

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,21 +1,35 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+import axios from "axios";
 
 export default function LiveSearch(props) {
-  const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+	const [term, setTerm] = useState("");
+	const [results, setResults] = useState([]);
+	const [log, setLog] = useState({});
 
-  return (
-    <Fragment>
-      <header className="logo">
-        <img src="images/brand.png" alt="Brand" />
-      </header>
-      <main>
-        <SearchBar onSearch={term => setTerm(term)} />
-        <Results results={results} />
-      </main>
-    </Fragment>
-  );
+	useEffect(() => {
+		console.log(term);
+		axios
+			.get(
+				`https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
+			)
+			.then(response => {
+				console.log(response);
+				setResults(response.data.results);
+			});
+	}, [term]);
+
+	return (
+		<Fragment>
+			<header className="logo">
+				<img src="images/brand.png" alt="Brand" />
+			</header>
+			<main>
+				<SearchBar onSearch={term => setTerm(term)} />
+				{/* <Results results={results} /> */}
+			</main>
+		</Fragment>
+	);
 }
